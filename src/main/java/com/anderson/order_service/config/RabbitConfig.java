416c6d869c731e4d5e6792b3e5793346f619649b
Anderson B. David <ands.david@live.com>
2026-01-20 16:04:36 -0300

    Enhance RabbitConfig with exchange, routing key, and message converter setup

--- src/main/java/com/anderson/order_service/config/RabbitConfig.java
+++ src/main/java/com/anderson/order_service/config/RabbitConfig.java
@@ -1,6 +1,14 @@
 package com.anderson.order_service.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +16,38 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
 
     public static final String ORDER_QUEUE = "order.created.queue";
+    public static final String ORDER_EXCHANGE = "order.exchange";
+    public static final String ORDER_ROUTING_KEY = "order.created";
 
     @Bean
     public Queue orderCreatedQueue() {
         return new Queue(ORDER_QUEUE, true);
+    }
+
+    @Bean
+    public DirectExchange orderExchange() {
+        return new DirectExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding orderBinding() {
+        return BindingBuilder
+                .bind(orderCreatedQueue())
+                .to(orderExchange())
+                .with(ORDER_ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 }
